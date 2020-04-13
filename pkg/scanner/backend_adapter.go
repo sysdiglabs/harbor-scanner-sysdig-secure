f5f5459ff2db0bc681e5b5474e43a9f65a3fbdd8
Néstor Salceda <nestor.salceda@gmail.com>
2020-04-14 00:03:01 +0200

    feat(scanner): Forward errors coming from Sysdig Secure

--- pkg/scanner/backend_adapter.go
+++ pkg/scanner/backend_adapter.go
@@ -20,8 +20,11 @@ func NewBackendAdapter(client secure.Client) Adapter {
 func (s *backendAdapter) Scan(req harbor.ScanRequest) (harbor.ScanResponse, error) {
 	var result harbor.ScanResponse
 
-	response, _ := s.secureClient.AddImage(fmt.Sprintf("%s:%s", req.Artifact.Repository, req.Artifact.Tag))
-	result.Id = response.ImageDigest
+	response, err := s.secureClient.AddImage(fmt.Sprintf("%s:%s", req.Artifact.Repository, req.Artifact.Tag))
+	if err != nil {
+		return result, err
+	}
 
+	result.Id = response.ImageDigest
 	return result, nil
 }
--- pkg/scanner/backend_adapter_test.go
+++ pkg/scanner/backend_adapter_test.go
@@ -1,6 +1,8 @@
 package scanner_test
 
 import (
+	"errors"
+
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -13,6 +15,10 @@ import (
 
 const (
 	imageDigest = "an image digest"
+)
+
+var (
+	secureError = errors.New("an error from Sysdig Secure")
 )
 
 var _ = Describe("BackendAdapter", func() {
@@ -34,24 +40,37 @@ var _ = Describe("BackendAdapter", func() {
 
 	Context("when scanning an image", func() {
 		It("sends the repository and tag to Sysdig Secure", func() {
-			scanRequest := harbor.ScanRequest{
-				Registry: &harbor.Registry{
-					Url:           "",
-					Authorization: "",
-				},
-				Artifact: &harbor.Artifact{
-					Repository: "sysdig/agent",
-					//Digest:     "",
-					Tag:      "9.7.0",
-					MimeType: "application/vnd.docker.distribution.manifest.v2+json",
-				},
-			}
 			secureResponse := secure.ScanResponse{ImageDigest: imageDigest}
 			client.EXPECT().AddImage("sysdig/agent:9.7.0").Return(secureResponse, nil)
 
-			scanResponse, _ := backendAdapter.Scan(scanRequest)
+			scanResponse, _ := backendAdapter.Scan(scanRequest())
 
 			Expect(scanResponse).To(Equal(harbor.ScanResponse{Id: imageDigest}))
 		})
+
+		Context("when Secure returns an error", func() {
+			It("returns the error", func() {
+				client.EXPECT().AddImage("sysdig/agent:9.7.0").Return(secure.ScanResponse{}, secureError)
+
+				_, err := backendAdapter.Scan(scanRequest())
+
+				Expect(err).To(MatchError(secureError))
+			})
+		})
 	})
 })
+
+func scanRequest() harbor.ScanRequest {
+	return harbor.ScanRequest{
+		Registry: &harbor.Registry{
+			Url:           "",
+			Authorization: "",
+		},
+		Artifact: &harbor.Artifact{
+			Repository: "sysdig/agent",
+			//Digest:     "",
+			Tag:      "9.7.0",
+			MimeType: "application/vnd.docker.distribution.manifest.v2+json",
+		},
+	}
+}
