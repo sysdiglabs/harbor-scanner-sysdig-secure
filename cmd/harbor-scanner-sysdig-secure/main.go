b523daa85e0ab268fb3d858cbda3b664ba4b5d2d
Néstor Salceda <nestor.salceda@gmail.com>
2020-05-19 17:44:44 +0200

    feat(cmd): Allow to choose scanning adapter with an environment variable

--- cmd/harbor-scanner-sysdig-secure/main.go
+++ cmd/harbor-scanner-sysdig-secure/main.go
@@ -4,6 +4,10 @@ import (
 	"os"
 
 	log "github.com/sirupsen/logrus"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
 	"github.com/sysdiglabs/harbor-scanner-sysdig-secure/pkg/http/api"
 	v1 "github.com/sysdiglabs/harbor-scanner-sysdig-secure/pkg/http/api/v1"
 	"github.com/sysdiglabs/harbor-scanner-sysdig-secure/pkg/scanner"
@@ -13,15 +17,32 @@ import (
 func main() {
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.TraceLevel)
-
 	log.Info("Starting harbor-scanner-sysdig-secure")
 
-	apiHandler := v1.NewAPIHandler(
-		scanner.NewBackendAdapter(
-			secure.NewClient(os.Getenv("SECURE_API_TOKEN"), os.Getenv("SECURE_URL"))),
-		log.StandardLogger().Writer())
+	apiHandler := v1.NewAPIHandler(getAdapter(), log.StandardLogger().Writer())
 
 	apiServer := api.NewServer(apiHandler)
 
 	log.Fatal(apiServer.ListenAndServe())
+}
+
+func getAdapter() scanner.Adapter {
+	client := secure.NewClient(os.Getenv("SECURE_API_TOKEN"), os.Getenv("SECURE_URL"))
+
+	if _, ok := os.LookupEnv("INLINE_SCANNING"); ok {
+		log.Info("Using inline-scanning adapter")
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return scanner.NewInlineAdapter(client, clientset, "harbor-scanner-sysdig-secure")
+	}
+
+	log.Info("Using backend-scanning adapter")
+	return scanner.NewBackendAdapter(client)
 }
