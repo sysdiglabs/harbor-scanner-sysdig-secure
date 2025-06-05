9dc6ce994f4a7aaa78c01cac0624924b29f6ead7
Fede Barcelona <fede_rico_94@hotmail.com>
2025-06-05 12:38:17 +0200

    build(nix): reuse options in docker image from dockerfile

--- docker.nix
+++ docker.nix
@@ -3,5 +3,13 @@ dockerTools.buildLayeredImage {
   name = "sysdiglabs/harbor-scanner-sysdig-secure";
   tag = harbor-adapter.version;
   contents = [ harbor-adapter ];
-  config.Entrypoint = "harbor-scanner-sysdig-secure";
+
+  # https://github.com/moby/moby/blob/46f7ab808b9504d735d600e259ca0723f76fb164/image/spec/spec.md#image-json-field-descriptions
+  config = {
+    Cmd = "/bin/harbor-scanner-sysdig-secure";
+    User = "1000:1000";
+    ExposedPorts = {
+      "5000" = { };
+    };
+  };
 }
