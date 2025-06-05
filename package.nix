a77cb21cd30f40b8857dc9bc458e2b5b406b292d
Fede Barcelona <fede_rico_94@hotmail.com>
2025-06-05 12:37:16 +0200

    build(nix): strip binary and make it static

--- package.nix
+++ package.nix
@@ -4,6 +4,13 @@ buildGoModule {
   version = "0.8.0";
   vendorHash = "sha256-da3up+9QRZR5oJokSGP89we6W2vku+ZdjQvL3hVfbpg=";
   src = ./.;
-
+  subPackages = [
+    "cmd/harbor-scanner-sysdig-secure"
+  ];
+  ldflags = [
+    "-w"
+    "-s"
+  ];
   doCheck = false;
+  env.CGO_ENABLED = 0;
 }
