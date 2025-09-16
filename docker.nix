01bd4d715b29b5f45678db09add5bae1976d255a
Fede Barcelona <fede_rico_94@hotmail.com>
2025-09-16 10:23:22 +0200

    build: make cmd a list of strings

--- docker.nix
+++ docker.nix
@@ -6,7 +6,7 @@ dockerTools.buildLayeredImage {
 
   # https://github.com/moby/moby/blob/46f7ab808b9504d735d600e259ca0723f76fb164/image/spec/spec.md#image-json-field-descriptions
   config = {
-    Cmd = "/bin/harbor-scanner-sysdig-secure";
+    Cmd = [ "/bin/harbor-scanner-sysdig-secure" ];
     User = "1000:1000";
     ExposedPorts = {
       "5000" = { };
