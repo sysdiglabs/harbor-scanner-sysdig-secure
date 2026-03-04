5b8872b8fd2d43e69d9a7d03e3bd8ef2dec90de2
Fede Barcelona <fede_rico_94@hotmail.com>
2026-03-04 12:07:41 +0100

    chore: update vendorHash in package.nix

--- package.nix
+++ package.nix
@@ -2,7 +2,7 @@
 buildGoModule {
   pname = "harbor-scanner-sysdig-secure";
   version = "0.8.4";
-  vendorHash = "sha256-wCvlsCGJjgo/ZJrvlJ/cg5KSnz05fjYi6cwCW/Opn/4=";
+  vendorHash = "sha256-zTLTbNYohQkYpvO8BDnFXczlcWABCvhwk/dRFmZuUqk=";
   src = ./.;
   subPackages = [
     "cmd/harbor-scanner-sysdig-secure"
