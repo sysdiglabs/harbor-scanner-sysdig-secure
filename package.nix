32df2ff23bf6af8fd6b84661ce5ffbd0139ff7e5
Fede Barcelona <fede_rico_94@hotmail.com>
2026-04-07 11:00:59 +0200

    chore: update dependencies 2026-04-07

--- package.nix
+++ package.nix
@@ -2,7 +2,7 @@
 buildGoModule {
   pname = "harbor-scanner-sysdig-secure";
   version = "0.8.4";
-  vendorHash = "sha256-zTLTbNYohQkYpvO8BDnFXczlcWABCvhwk/dRFmZuUqk=";
+  vendorHash = "sha256-xcdnL/1AUyQ0n3R5rLVxpv/Mk9cbJa5PWqnbYY2Q5iQ=";
   src = ./.;
   subPackages = [
     "cmd/harbor-scanner-sysdig-secure"
